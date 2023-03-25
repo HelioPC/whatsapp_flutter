294104d3292e3d1b1646838c9dac3289c39a650e
Eliúde Vemba <sweeteliude@gmail.com>
2023-03-25 12:30:00 +0100

    Configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_flutter/common/themes/dark_theme.dart';
 import 'package:whatsapp_flutter/common/themes/light_theme.dart';
 import 'package:whatsapp_flutter/feature/auth/pages/user_info_page.dart';
+import 'package:whatsapp_flutter/firebase_options.dart';
 // import 'package:whatsapp_flutter/feature/auth/pages/login_page.dart';
 // import 'package:whatsapp_flutter/feature/auth/pages/verification_page.dart';
 // import 'package:whatsapp_flutter/feature/welcome/pages/welcome_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
