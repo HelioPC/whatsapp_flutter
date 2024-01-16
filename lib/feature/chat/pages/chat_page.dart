d656772785483b40dfc24f4ae82c297d487625cd
Eliúde Vemba <sweeteliude@gmail.com>
2024-01-16 20:14:05 +0100

    Add page storage to chat page list
    to avoid page reset on enter

--- lib/feature/chat/pages/chat_page.dart
+++ lib/feature/chat/pages/chat_page.dart
@@ -13,6 +13,8 @@ import 'package:whatsapp_flutter/feature/auth/widgets/custom_icon_button.dart';
 import 'package:whatsapp_flutter/feature/chat/controllers/chat_controller.dart';
 import 'package:whatsapp_flutter/feature/chat/widgets/chat_text_field.dart';
 
+final pageStorageBucket = PageStorageBucket();
+
 class ChatPage extends ConsumerWidget {
   final UserModel userModel;
 
@@ -132,125 +134,132 @@ class ChatPage extends ConsumerWidget {
 
                     final messages = snapshot.data;
 
-                    return ListView.builder(
-                      itemCount: messages!.length,
-                      shrinkWrap: true,
-                      controller: _scrollController,
-                      itemBuilder: (_, index) {
-                        final message = messages[index];
-                        final isSender = message.senderId ==
-                            FirebaseAuth.instance.currentUser!.uid;
+                    return PageStorage(
+                      bucket: pageStorageBucket,
+                      child: ListView.builder(
+                        key: const PageStorageKey('chat_page_list'),
+                        itemCount: messages!.length,
+                        shrinkWrap: true,
+                        controller: _scrollController,
+                        itemBuilder: (_, index) {
+                          final message = messages[index];
+                          final isSender = message.senderId ==
+                              FirebaseAuth.instance.currentUser!.uid;
 
-                        final haveNip = (index == 0) ||
-                            (index == messages.length - 1 &&
-                                message.senderId !=
-                                    messages[index - 1].senderId) ||
-                            (message.senderId != messages[index - 1].senderId &&
-                                message.senderId ==
-                                    messages[index + 1].senderId) ||
-                            (message.senderId != messages[index - 1].senderId &&
-                                message.senderId !=
-                                    messages[index + 1].senderId);
+                          final haveNip = (index == 0) ||
+                              (index == messages.length - 1 &&
+                                  message.senderId !=
+                                      messages[index - 1].senderId) ||
+                              (message.senderId !=
+                                      messages[index - 1].senderId &&
+                                  message.senderId ==
+                                      messages[index + 1].senderId) ||
+                              (message.senderId !=
+                                      messages[index - 1].senderId &&
+                                  message.senderId !=
+                                      messages[index + 1].senderId);
 
-                        return Column(
-                          children: [
-                            if (index == 0)
-                              Container(
-                                margin: const EdgeInsets.symmetric(
-                                  vertical: 10,
-                                  horizontal: 30,
-                                ),
-                                padding: const EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                  color: context.theme.yellowCardBgColor,
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: Text(
-                                  'Messages and calls are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them. Tap to learn more.',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 13,
-                                    color: context.theme.yelloCardTextColor,
+                          return Column(
+                            children: [
+                              if (index == 0)
+                                Container(
+                                  margin: const EdgeInsets.symmetric(
+                                    vertical: 10,
+                                    horizontal: 30,
                                   ),
-                                ),
-                              ),
-                            Container(
-                              alignment: isSender
-                                  ? Alignment.centerRight
-                                  : Alignment.centerLeft,
-                              margin: EdgeInsets.only(
-                                top: 4,
-                                bottom: 4,
-                                left: isSender
-                                    ? 80
-                                    : haveNip
-                                        ? 10
-                                        : 15,
-                                right: isSender
-                                    ? haveNip
-                                        ? 10
-                                        : 15
-                                    : 80,
-                              ),
-                              child: ClipPath(
-                                clipper: haveNip
-                                    ? UpperNipMessageClipperTwo(
-                                        isSender
-                                            ? MessageType.send
-                                            : MessageType.receive,
-                                        nipWidth: 8,
-                                        nipHeight: 10,
-                                        bubbleRadius: haveNip ? 12 : 0,
-                                      )
-                                    : null,
-                                child: Container(
-                                  padding: EdgeInsets.only(
-                                    left: isSender ? 10 : 15,
-                                    right: isSender ? 15 : 10,
-                                    top: 8,
-                                    bottom: 8,
-                                  ),
+                                  padding: const EdgeInsets.all(10),
                                   decoration: BoxDecoration(
-                                    color: isSender
-                                        ? context.theme.senderChatCardBg
-                                        : context.theme.receiverChatCardBg,
-                                    borderRadius: haveNip
-                                        ? null
-                                        : BorderRadius.circular(10),
-                                    boxShadow: const [
-                                      BoxShadow(color: Colors.black38)
-                                    ],
+                                    color: context.theme.yellowCardBgColor,
+                                    borderRadius: BorderRadius.circular(10),
                                   ),
-                                  child: Stack(
-                                    children: [
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(bottom: 5),
-                                        child: Text(
-                                          '${message.textMessage}            ',
-                                          style: const TextStyle(fontSize: 16),
-                                        ),
-                                      ),
-                                      Positioned(
-                                        bottom: 0,
-                                        right: 0,
-                                        child: Text(
-                                          DateFormat.Hm()
-                                              .format(message.timeSent),
-                                          style: TextStyle(
-                                            fontSize: 11,
-                                            color: context.theme.greyColor,
+                                  child: Text(
+                                    'Messages and calls are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them. Tap to learn more.',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 13,
+                                      color: context.theme.yelloCardTextColor,
+                                    ),
+                                  ),
+                                ),
+                              Container(
+                                alignment: isSender
+                                    ? Alignment.centerRight
+                                    : Alignment.centerLeft,
+                                margin: EdgeInsets.only(
+                                  top: 4,
+                                  bottom: 4,
+                                  left: isSender
+                                      ? 80
+                                      : haveNip
+                                          ? 10
+                                          : 15,
+                                  right: isSender
+                                      ? haveNip
+                                          ? 10
+                                          : 15
+                                      : 80,
+                                ),
+                                child: ClipPath(
+                                  clipper: haveNip
+                                      ? UpperNipMessageClipperTwo(
+                                          isSender
+                                              ? MessageType.send
+                                              : MessageType.receive,
+                                          nipWidth: 8,
+                                          nipHeight: 10,
+                                          bubbleRadius: haveNip ? 12 : 0,
+                                        )
+                                      : null,
+                                  child: Container(
+                                    padding: EdgeInsets.only(
+                                      left: isSender ? 10 : 15,
+                                      right: isSender ? 15 : 10,
+                                      top: 8,
+                                      bottom: 8,
+                                    ),
+                                    decoration: BoxDecoration(
+                                      color: isSender
+                                          ? context.theme.senderChatCardBg
+                                          : context.theme.receiverChatCardBg,
+                                      borderRadius: haveNip
+                                          ? null
+                                          : BorderRadius.circular(10),
+                                      boxShadow: const [
+                                        BoxShadow(color: Colors.black38)
+                                      ],
+                                    ),
+                                    child: Stack(
+                                      children: [
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(bottom: 5),
+                                          child: Text(
+                                            '${message.textMessage}            ',
+                                            style:
+                                                const TextStyle(fontSize: 16),
                                           ),
                                         ),
-                                      ),
-                                    ],
+                                        Positioned(
+                                          bottom: 0,
+                                          right: 0,
+                                          child: Text(
+                                            DateFormat.Hm()
+                                                .format(message.timeSent),
+                                            style: TextStyle(
+                                              fontSize: 11,
+                                              color: context.theme.greyColor,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          ],
-                        );
-                      },
+                            ],
+                          );
+                        },
+                      ),
                     );
                   },
                 ),
